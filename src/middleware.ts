4adf9ac77ed57ac23a57662ac6b23dbb16ec8a0e
sivanmah <sivanmahmud@gmail.com>
2024-10-23 17:49:37 +0200

    added display-name cookie

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+} from "unique-names-generator";
+
+export function middleware(req: NextRequest) {
+  const randomName = uniqueNamesGenerator({
+    dictionaries: [adjectives, colors, animals],
+    separator: "",
+    style: "capital",
+  });
+
+  const res = NextResponse.next();
+
+  const existingCookie = req.cookies.get("display-name");
+
+  if (!existingCookie) {
+    res.cookies.set("display-name", randomName, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: "/",
+};
