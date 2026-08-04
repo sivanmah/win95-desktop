ee7f5034e886995dd9d7d73ce990bf32ba854cf9
sivanmah <sivanmahmud@gmail.com>
2026-08-04 13:14:55 +0200

    improved start menu

--- src/app/components/StartMenu.tsx
+++ src/app/components/StartMenu.tsx
@@ -8,7 +8,7 @@ export default function StartMenu({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed bottom-8 left-0 w-64 bg-gray-300 border-t-2 border-l-2 border-white border-b-2 border-r-2 border-gray-800 shadow-lg">
+    <div className="fixed bottom-8 left-0 w-64 bg-gray-300 border-2 border-t-white border-l-white border-b-black border-r-black">
       <div className="bg-blue-800 h-full w-8 absolute left-0 top-0"></div>
       <ul className="ml-10 py-2">
         <li
@@ -19,6 +19,24 @@ export default function StartMenu({
           }}
         >
           Notepad
+        </li>
+        <li
+          className="px-4 py-2 hover:bg-blue-600 hover:text-white cursor-pointer"
+          onClick={() => {
+            // onAppClick("notepad");
+            onClose();
+          }}
+        >
+          Chatbot
+        </li>
+        <li
+          className="px-4 py-2 hover:bg-blue-600 hover:text-white cursor-pointer"
+          onClick={() => {
+            // onAppClick("notepad");
+            onClose();
+          }}
+        >
+          Chatroom
         </li>
         {/* Add more menu items as needed */}
       </ul>
